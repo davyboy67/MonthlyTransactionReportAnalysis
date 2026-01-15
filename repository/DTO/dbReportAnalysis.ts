0351b7eeaa5bec1d32a49dd616ed2d4c77b49b7a
davyboy67 <davidoduntan2@gmail.com>
2026-01-15 19:08:26 +0200

    cleanup

--- repository/DTO/dbReportAnalysis.ts
+++
@@ -1,8 +0,0 @@
-import { DBTransaction } from "./dbTransaction";
-
-export interface DbReportAnalysis {
-    id: string;
-    report_date: Date;
-    totalIncome: number;
-    totalExpenses: number;
-}
--- repository/IReportAnalysisRepo.ts
+++
@@ -1,8 +0,0 @@
-import { IReportAnalysis } from "../models/IReportAnalysis";
-import { ITransaction } from "../models/ITransaction";
-import { DbReportAnalysis } from "./DTO/dbReportAnalysis";
-
-export interface IReportAnalysisRepo {
-    getReportAnalysis(reportDate: Date): Promise<DbReportAnalysis>;
-    saveReportAnalysis(reportAnalysis: IReportAnalysis): Promise<void>;
-}
