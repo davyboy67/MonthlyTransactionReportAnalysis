9b58317e7b9c33909ef16305047822dd62af00d2
davyboy67 <davidoduntan2@gmail.com>
2026-03-16 19:27:30 +0200

    UI Update: V1

--- packages/frontend/src/components/organisms/categorySummary.tsx
+++ packages/frontend/src/components/organisms/categorySummary.tsx
@@ -1,35 +1,44 @@
 import type { ICategorySummary } from "../../types";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 interface CategorySummaryProps {
-    summaries: ICategorySummary[];
+  summaries: ICategorySummary[];
 }
 
 export function CategorySummary({ summaries }: CategorySummaryProps) {
-    return (
-        <div>
-            <h2>Category Summary</h2>
-            <div style={{ height: '400px', width: '600px'}}>
-                <ResponsiveContainer width="100%" height="100%">
-                    <BarChart
-                        data={summaries}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="budget" fill="#8884d8" />
-                        <Bar dataKey="expenditure" fill="#82ca9d" />
-                    </BarChart>
-                </ResponsiveContainer>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Category Summary</h2>
+      <div style={{ height: "400px", width: "600px" }}>
+        <ResponsiveContainer width="100%" height={500}>
+          <BarChart
+            data={summaries}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="budget" fill="#8884d8" />
+            <Bar dataKey="expenditure" fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 }
--- packages/frontend/src/components/organisms/monthlyOverview.tsx
+++ packages/frontend/src/components/organisms/monthlyOverview.tsx
@@ -1,39 +1,89 @@
 import type { IMonthlySummary } from "../../types";
 import { MetricTile } from "../molecules/metricTile";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
 
 type Props = {
-    summary: IMonthlySummary;
-}
+  summary: IMonthlySummary;
+};
 
 export function MonthlyOverview({ summary }: Props) {
-    const data = [
-        { name: 'Income', value: summary.totalIncome, fill: '#1f57ff' },
-        { name: 'Expenses', value: summary.totalExpenses, fill: '#0073ad' },
-        { name: 'Savings', value: summary.totalSavings, fill: '#28a745' },
-    ];
+  const data = [
+    { name: "Income", value: summary.totalIncome },
+    { name: "Expenses", value: summary.totalExpenses },
+    { name: "Savings", value: summary.totalSavings },
+  ];
 
-    return (
+  const colours = ["#035700", "#680202", "#0003a0"];
+
+  return (
     <section>
-      <h2>{summary.month} Overview</h2>
+      {/* <h2>{summary.month} Overview</h2>
 
-      <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gap: "1rem" }}>
-        <MetricTile label="Income" value={`R ${summary.totalIncome.toFixed(2)}`} />
-        <MetricTile label="Expenses" value={`R ${summary.totalExpenses.toFixed(2)}`} />
-        <MetricTile label="Savings" value={`R ${summary.totalSavings.toFixed(2)}`} />
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          gap: "1rem",
+        }}
+      >
+        <MetricTile
+          label="Income"
+          value={`R ${summary.totalIncome.toFixed(2)}`}
+        />
+        <MetricTile
+          label="Expenses"
+          value={`R ${summary.totalExpenses.toFixed(2)}`}
+        />
+        <MetricTile
+          label="Savings"
+          value={`R ${summary.totalSavings.toFixed(2)}`}
+        />
       </div>
 
       <div style={{ marginTop: "2rem", height: "300px" }}>
         <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={data}>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="value" />
-            </BarChart>
+          <BarChart data={data}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="value" />
+          </BarChart>
         </ResponsiveContainer>
-      </div>
+      </div> */}
+
+      <ResponsiveContainer width="100%" height={600}>
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={"80%"}
+            fill="#8884d8"
+            label={({ name, value }) => `${name}: ${value}`}
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={colours[index % colours.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
     </section>
   );
 }
